0d14882ce3a0eeaa503adb09b5525c9ac3663d8b
Memmedli Hikmet <memmedlih032@gmail.com>
2024-08-18 21:34:32 +0400

    stars functionality

---
+++ index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function () {
+  loadStars();
+});
+
+let all_stars = document.querySelectorAll(".star");
+
+all_stars.forEach((star, index) => {
+  star.addEventListener("mouseover", function (e) {
+    if (e.currentTarget.classList.contains("filled")) return;
+    let previous = e.currentTarget.previousElementSibling;
+    e.currentTarget.firstElementChild.className = "fa-solid fa-star";
+    e.currentTarget.classList.add("shadow");
+    for (let i = 0; i < index; i++) {
+      previous.classList.add("shadow");
+      previous.firstElementChild.className = "fa-solid fa-star";
+      previous = previous.previousElementSibling;
+    }
+  });
+  star.addEventListener("mouseout", function (e) {
+    let previous = e.currentTarget.previousElementSibling;
+    e.currentTarget.classList.remove("shadow");
+    for (let i = 0; i < index; i++) {
+      previous.classList.remove("shadow");
+      previous = previous.previousElementSibling;
+    }
+    loadStars();
+  });
+  star.addEventListener("click", function (e) {
+    localStorage.setItem("star_count", index + 1);
+    loadStars();
+  });
+});
+
+function loadStars() {
+  let star_count = +localStorage.getItem("star_count");
+  all_stars.forEach((star, index) => {
+    if (index < star_count) {
+      star.firstElementChild.className = "fa-solid fa-star";
+      star.classList.remove("shadow");
+      star.classList.add("filled");
+    } else {
+      star.firstElementChild.className = "fa-regular fa-star";
+
+      star.classList.remove("filled");
+    }
+  });
+}
